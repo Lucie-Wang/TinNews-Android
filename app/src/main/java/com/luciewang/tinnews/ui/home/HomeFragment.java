55972850c0124deba760ace6d5a37ffc46c11008
Lucie Wang <lucie.wangxy@gmail.com>
2020-10-16 11:51:57 -0700

    fixed like feature bug

--- app/src/main/java/com/luciewang/tinnews/ui/home/HomeFragment.java
+++ app/src/main/java/com/luciewang/tinnews/ui/home/HomeFragment.java
@@ -101,10 +101,10 @@ public class HomeFragment extends Fragment implements CardStackListener {
         if (direction == Direction.Left) {
             Log.d("CardStackView", "Unliked " + layoutManager.getTopPosition());
         } else if (direction == Direction.Right) {
-            Log.d("CardStackView", "Liked "  + layoutManager.getTopPosition());
+            Log.d("CardStackView", "Liked " + layoutManager.getTopPosition());
+            Article article = articles.get(layoutManager.getTopPosition() - 1);
+            viewModel.setFavoriteArticleInput(article);
         }
-        Article article = articles.get(layoutManager.getTopPosition() -1);
-        viewModel.setFavoriteArticleInput(article);
     }
 
     @Override
